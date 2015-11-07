ace434f5dbdbc3aa333ee8bd6ba6a59e2d7cf948
Alice Pote <alice.writes.wrongs@gmail.com>
2015-11-07 14:13:23 -0500

    devise config

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,6 +1,5 @@
 class AuthorsController < ApplicationController
   before_filter :authenticate_user! 
-  load_and_authorize_resource
 
   # GET /authors
   # GET /authors.json
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!
-  load_and_authorize_resource 
 
   def index
     @users = User.all
@@ -22,7 +21,7 @@ class UsersController < ApplicationController
       redirect_to users_path, :alert => "Unable to update user."
     end
   end
-    
+
   def destroy
     user = User.find(params[:id])
     unless user == current_user
