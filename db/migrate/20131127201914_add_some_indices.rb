fada9574cd1efc2e27e229944c813aa3f7a76c56
Alice Pote <alice.writes.wrongs@gmail.com>
2015-11-08 17:28:09 -0500

    no posts

--- db/migrate/20131127201914_add_some_indices.rb
+++ db/migrate/20131127201914_add_some_indices.rb
@@ -1,7 +1,6 @@
     class AddSomeIndices < ActiveRecord::Migration
       def change
         add_index :purchase_orders, :owner_id
-        add_index :posts, :post_category_id
         add_index :images, [:imagey_id, :imagey_type]
         add_index :contributions, :author_id
         add_index :contributions, :title_id
