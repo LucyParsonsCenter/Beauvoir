9b7785a9ebc2cbff6041cf4ded629d56a21204af
Alice Pote <alice.writes.wrongs@gmail.com>
2015-11-11 15:43:27 -0500

    removing image dealio

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,9 +4,7 @@ class Category < ActiveRecord::Base
 
   has_many :category_title_list_memberships
   has_many :title_lists, :through => :category_title_list_memberships
-  accepts_nested_attributes_for :category_title_list_memberships, :allow_destroy => true  
-
-  mount_uploader :image, ImageUploader
+  accepts_nested_attributes_for :category_title_list_memberships, :allow_destroy => true
 
   def titles_in_stock
     titles.find_all {|t| t.in_stock > 0 }.length
