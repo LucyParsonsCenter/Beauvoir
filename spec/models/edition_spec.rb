450d9f5067c880a0624605d4c58ea32e9e291e63
Alice Pote <alice.writes.wrongs@gmail.com>
2015-11-11 17:15:55 -0500

    don't warn about false positives

---
+++ spec/models/edition_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Edition, type: :model do
+  describe "basic tests" do
+    before :each do
+      @edition = FactoryGirl.create(:edition)
+    end
+
+    it "should let you create an edition" do
+      expect(@edition.notes).to eq("wow, a book!")
+      expect(@edition.list_price_cents).to eq(230)
+    end
+
+    it "should throw an error if the ISBN is invalid" do
+      expect { FactoryGirl.create(:edition, isbn10: "1234567890") }.to raise_error
+        # to raise_error(ActiveRecord::RecordInvalid:Validation failed, "Isbn10 not valid")
+    end
+  end
+end
