d743eb081b97809e99fa709175998a7d618cad6d
Alice Pote <alice.writes.wrongs@gmail.com>
2015-11-11 16:03:01 -0500

    simple tests for distributors

---
+++ spec/models/distributor_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Distributor, type: :model do
+  describe "basic tests" do
+    before :each do
+      @distributor = FactoryGirl.create(:distributor)
+    end
+
+    it "should be able to create a distributor" do
+      expect(@distributor.name).to eq("AK Press")
+      expect(@distributor.notes).to eq("wow what nice books you have")
+      expect(@distributor.our_account_number).to eq("1234567890")
+      expect(@distributor.to_s).to eq("AK Press (1)")
+    end
+
+  end
+end
