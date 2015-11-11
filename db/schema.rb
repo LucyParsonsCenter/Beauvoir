4bf6d696ba366fd78e5597e87aa348940599a60a
Alice Pote <alice.writes.wrongs@gmail.com>
2015-11-11 15:46:25 -0500

    schema after removing images

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151111201758) do
+ActiveRecord::Schema.define(version: 20151111204249) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,7 +28,6 @@ ActiveRecord::Schema.define(version: 20151111201758) do
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
-    t.string   "image"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
