0db543783fd2337afb80106b587fa14354afae5e
Alice Pote <alice.writes.wrongs@gmail.com>
2015-11-11 10:16:45 -0500

    rspec config for factory_girl

--- config/application.rb
+++ config/application.rb
@@ -1,27 +1,19 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-require 'csv'
 
 Bundler.require(*Rails.groups)
 
-module Borges
+module Beauvoir
   class Application < Rails::Application
 
     # don't generate RSpec tests for views and helpers
     config.generators do |g|
-
       g.test_framework :rspec, fixture: true
-
-      g.fixture_replacement :machinist
-
+      g.fixture_replacement :factory_girl, dir: "spec/factories"
       g.view_specs false
       g.helper_specs false
     end
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
