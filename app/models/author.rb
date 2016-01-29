29f9b1c68413c77c35115a27f483e59705c0c5fb
Alice Pote <alice.writes.wrongs@gmail.com>
2016-01-29 08:13:32 -0500

    habtm on author, title

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,6 +1,5 @@
 class Author < ApplicationRecord
-  has_many :authorships
-  has_many :titles, through: :authorships
+  has_and_belongs_to_many :titles
 
   def to_s
     "#{self.first_name} #{self.last_name}"
--- app/models/title.rb
+++ app/models/title.rb
@@ -1,2 +1,4 @@
 class Title < ApplicationRecord
+  has_and_belongs_to_many :authors
+
 end
