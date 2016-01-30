2e6ed5e6395d4fb3fefba96a4fd8219967b41ed4
Alice Pote <alice.writes.wrongs@gmail.com>
2016-01-29 22:05:55 -0500

    associations on titles and categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,7 @@
 class Category < ApplicationRecord
+  has_and_belongs_to_many :titles
+
+  def to_s
+    self.name
+  end
 end
--- app/models/title.rb
+++ app/models/title.rb
@@ -1,4 +1,8 @@
 class Title < ApplicationRecord
   has_and_belongs_to_many :authors
+  has_and_belongs_to_many :categories
 
+  def to_s
+    self.name
+  end
 end
