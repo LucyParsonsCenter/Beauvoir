357de1d4a6108321424abb7b1532389c22bd3e51
Alice Pote <alice.writes.wrongs@gmail.com>
2016-01-29 22:51:48 -0500

    one edition spec

--- spec/models/edition_spec.rb
+++ spec/models/edition_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Edition, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should have a valid factory" do
+    expect(FactoryGirl.create(:edition)).to be_valid
+  end
 end
