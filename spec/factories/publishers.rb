d99db65056d9041bfe65cd1bcac8003bd80fa357
Alice Pote <alice.writes.wrongs@gmail.com>
2016-01-29 22:48:09 -0500

    wow!

--- spec/factories/publishers.rb
+++ spec/factories/publishers.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :publisher do
-    
+    name "AK Press"
+    city "Oakland CA"
   end
-
 end
--- spec/models/publisher_spec.rb
+++ spec/models/publisher_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Publisher, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should have a valid factory" do
+    expect(FactoryGirl.create(:publisher)).to be_valid
+  end
 end
